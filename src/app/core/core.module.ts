97249da2d94a02759e3aca52ce68825732aaf3f2
Siarhei Sayevich <siarhei_sayevich@epam.com>
2018-11-30 16:25:24 +0300

    move logo & courses-item components inside respective parents

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { FooterComponent } from './footer/footer.component';
-import { LogoComponent } from './logo/logo.component';
+import { LogoComponent } from './header/logo/logo.component';
 
 @NgModule({
   declarations: [HeaderComponent, BreadcrumbsComponent, FooterComponent, LogoComponent],
--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesListComponent } from './courses-list/courses-list.component';
-import { CoursesItemComponent } from './courses-item/courses-item.component';
+import { CoursesItemComponent } from './courses-list/courses-item/courses-item.component';
 
 @NgModule({
   declarations: [CoursesListComponent, CoursesItemComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [CoursesListComponent]
 })
 export class CoursesModule { }
