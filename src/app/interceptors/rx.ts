f84c1dfad5416f5436394164d60afa84925323ae
volshibenik <volshibenik@gmail.com>
2019-02-14 19:52:40 +0300

    test

--- src/app/interceptors/rx.ts
+++ src/app/interceptors/rx.ts
@@ -1,5 +1,5 @@
-/* const { Observable } = require("rxjs")
-
+const { Observable } = require('rxjs');
+/*
 function inter(v) {
   return new Observable(obr => {
     let n = 0;
@@ -18,3 +18,45 @@ function inter(v) {
 
 
  */
+
+const listen = obr => {
+  obr.next(1);
+  obr.next(11);
+  obr.next(111);
+};
+
+const kk = new Observable(listen);
+kk.subscribe(d => console.log(d));
+
+/* function evH(el, event) {
+  return new Observable(obr => {
+    const handler = e => obr.next(e);
+    el.addEventListener(event, handler);
+
+    return {
+      unsubscribe() {
+        // el.removeEventLis
+      },
+    };
+  });
+}
+
+const key = 25;
+const handler = evH('btn', 'kaydown');
+handler.subscribe(e => {
+  if ((e.keyKode = 25)) {
+    'BAM';
+  }
+});
+ */
+
+function delayed(obr) {
+  obr.next(1);
+  setTimeout(() => {
+    obr.next(2);
+  }, 1000);
+  setTimeout(() => {
+    obr.next(3);
+  }, 2000);
+}
+new Observable(delayed).subscribe(e => console.log(`${new Date()} ` + e));
--- src/app/kkk/kkk.component.ts
+++ src/app/kkk/kkk.component.ts
@@ -9,36 +9,39 @@ function fromE(el, event) {
 
     return {
       unsubscribe() {
-        el.removeEventListener(event, eventH, false)
-      }
-    }
-  })
+        el.removeEventListener(event, eventH, false);
+      },
+    };
+  });
 }
+
+const listen = obr => {
+  obr.next(1);
+  obr.next(11);
+  obr.next(111);
+};
 
 @Component({
   selector: 'app-kkk',
   template: `
-    <p class="lll" >
+    <p class="lll">
       kkk works!
     </p>
   `,
-  styleUrls: ['./kkk.component.scss']
+  styleUrls: ['./kkk.component.scss'],
 })
 export class KkkComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     const pp = document.querySelector('.lll');
-    fromE(pp, 'click').subscribe(e => console.log(e))
+    fromE(pp, 'click').subscribe(e => console.log(e));
 
     const mappedNums = from([400, 400, 12, 12, 3, 1]).pipe(
       map(n => n + 2),
-      distinctUntilChanged()
-    )
+      distinctUntilChanged(),
+    );
 
-    mappedNums.subscribe(e => console.log(e))
-
+    mappedNums.subscribe(e => console.log(e));
   }
-
 }
